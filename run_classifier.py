db48c84e97a094754b2653df544d67763a8545e4
StefanLyu <80801770+StefanLyu@users.noreply.github.com>
2021-03-17 16:36:45 +0800

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -505,7 +505,9 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   assert len(input_ids) == max_seq_length
   assert len(input_mask) == max_seq_length
   assert len(segment_ids) == max_seq_length
-  tf.logging.info(label_map)
+  print(label_map)
+  print("start")
+  tf.logging.info("Start")
   label_id = label_map[str(example.label)]
   if ex_index < 5:
     tf.logging.info("*** Example ***")
